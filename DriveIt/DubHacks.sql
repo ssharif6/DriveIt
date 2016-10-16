5b3c6c930309554e9a1e38952274817cb111b556
max <max.shao@hotmail.com>
2016-10-15 20:20:18 -0700

    Adding DubHacks.sql to Project

---
+++ DriveIt/DubHacks.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE dbo.Users (
+	UserId INTEGER PRIMARY KEY,
+	FirstName VARCHAR(25),
+	LastName VARCHAR(25),
+	Rating INTEGER 
+);
+
+CREATE TABLE dbo.Histories (
+	HistoryId INTEGER PRIMARY KEY
+);
