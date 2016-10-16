af90893c01651926630494dfc0d3766aaa2c3207
iguest <iguest@IS-M334L23-22>
2016-10-16 14:57:48 -0700

    Last push

--- DriveIt/Managers/CarInfoManager.cs
+++ DriveIt/Managers/CarInfoManager.cs
@@ -13,10 +13,10 @@ namespace DriveIt.Managers
         public static string _getUserCarInfoById = @"SELECT h1.PId, PidTable.Descriptions, PidTable.Units, Users.FirstName, Users.LastName, Users.Age, Cars.Make, Cars.Model, Cars.Year, Cars.IsHybrid, AVG(h1.Value) AS Value,
 	(SELECT AVG(h2.Value) AS NationalValue 
 	FROM Histories h2 
-	WHERE h1.PId = h2.PId AND h2.CarId = @carId
+	WHERE h1.PId = h2.PId AND h2.UserId = @userId
 	GROUP BY h2.PId) AS NationalValue
 FROM Histories h1, PidTable, Cars, Users 
-WHERE PidTable.PId = h1.PId AND h1.UserId = Users.UserId AND h1.UserId = @userId AND h1.CarId = @carId 
+WHERE PidTable.PId = h1.PId AND h1.UserId = Users.UserId AND h1.UserId = @userId AND h1.CarId = @carId AND Cars.CarId = @carId
 GROUP BY h1.PId, PidTable.Descriptions, PidTable.Units, Users.FirstName, Users.LastName, Users.Age, Cars.Make, Cars.Model, Cars.Year, Cars.IsHybrid;";
 
         public int AddCarid(int carId)
