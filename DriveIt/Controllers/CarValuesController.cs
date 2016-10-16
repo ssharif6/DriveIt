659cd8e90f88ab0dcfd08f2ab488bc38fba3d9ab
iGuest <iGuest>
2016-10-15 23:36:05 -0700

    Added base

---
+++ DriveIt/Controllers/CarValuesController.cs
@@ -0,0 +1,43 @@
+﻿using DriveIt.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DriveIt.Controllers
+{
+    public class CarValuesController : Controller
+    {
+        
+        // GET: CarValues
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public CarOutputModel Post(int userId, int pid, int value)
+        {
+            // this should talk to the PID DB to get actual information given the pid
+            var pidModel = new PidModel
+            {
+                Description = "blah",
+                MaxValue = 255,
+                MinValue = 0,
+                Pid = pid,
+                Units = "units"
+            };            
+
+            var model = new CarOutputModel
+            {
+                PidModel = pidModel,
+
+            }
+        }
+
+        public GetUserInfo Get(int userId)
+        {
+
+        }
+    }
+}
---
+++ DriveIt/Managers/CarInfoManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace DriveIt.Managers
+{
+    public class CarInfoManager
+    {
+        private static string _connectionString = "Server=driveit.database.windows.net;Database=driveit;Trusted_Connection=true";
+
+        public static string _getUserCarInfoById = @"SELECT * FROM Histories WHERE UserId = @userId AND CarId = @carId";
+        public GetUserCarInfo(int userId, int carId)
+        {
+            var conn = withSqlConnection(_connectionString);
+            SqlCommand command = new SqlCommand(_getUserCarInfoById, conn);
+            command.Parameters.Add(new SqlParameter("userId", userId));
+            command.Parameters.Add(new SqlParameter("carId", carId));
+
+            // read Sql
+            using (SqlDataReader reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    Console.WriteLine(reader[0]);
+                }
+            }
+
+        }
+
+        private SqlConnection withSqlConnection(string connectionName)
+        {
+            using (SqlConnection conn = new SqlConnection())
+            {
+                conn.ConnectionString = connectionName;
+                conn.Open();
+                return conn;
+            }
+        }
+
+        private SqlDataReader readSql(SqlCommand command)
+        {
+            
+        }
+    }
+}
