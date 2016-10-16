f67f07b79d77f268f749a1a7e7975613266e2754
iguest <iguest@IS-M334L23-22>
2016-10-16 14:53:41 -0700

    Adding

--- DriveIt/Managers/CarInfoManager.cs
+++ DriveIt/Managers/CarInfoManager.cs
@@ -13,7 +13,7 @@ namespace DriveIt.Managers
         public static string _getUserCarInfoById = @"SELECT h1.PId, PidTable.Descriptions, PidTable.Units, Users.FirstName, Users.LastName, Users.Age, Cars.Make, Cars.Model, Cars.Year, Cars.IsHybrid, AVG(h1.Value) AS Value,
 	(SELECT AVG(h2.Value) AS NationalValue 
 	FROM Histories h2 
-	WHERE h1.PId = h2.PId AND h2.UserId = @userId
+	WHERE h1.PId = h2.PId AND h2.CarId = @carId
 	GROUP BY h2.PId) AS NationalValue
 FROM Histories h1, PidTable, Cars, Users 
 WHERE PidTable.PId = h1.PId AND h1.UserId = Users.UserId AND h1.UserId = @userId AND h1.CarId = @carId 
